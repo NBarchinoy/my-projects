1aaa596d806c36d03f1c561099a63d849eb42395
Barchinoy <126873220+NBarchinoy@users.noreply.github.com>
2023-03-06 02:21:21 +0500

    Add files via upload

---
+++ lesson-2.py
@@ -0,0 +1,16 @@
+#Theme:calculator
+#Date:06.03.2023
+#Creat:Normuhammadova
+a = int(input("Enter the first number:"))
+c = input("Enter the action symbol:")
+b = int(input("Enter the second number:"))
+if c == "+":
+    print(a+b)
+if c == "-":
+    print(a-b)
+if c == "*":
+    print(a*b)
+if c == "/":
+    print(a/b)
+if c == "**":
+    print(a**b)
