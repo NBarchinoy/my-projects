e7f9f270762ba8b3493b3891baba4b7cd670f751
Barchinoy <126873220+NBarchinoy@users.noreply.github.com>
2023-03-28 09:57:40 +0500

    Add files via upload

---
+++ homeWork.py
@@ -0,0 +1,32 @@
+# 1-VAZIFA
+#Ismlarga qarab kimligini aytish!
+
+# def relation_to_luke(name):
+# 	solutions = {'Leia':'sister',
+# 	'Darth Vader':'father',
+# 	'Han':'brother in law',
+# 	'R2D2':'droid'}
+# 	return 'Luke, I am your {}.'.format(solutions[name])
+
+
+# def relation_to_luke(name):
+#     if name == "Darth Vader":
+# 		    return "Luke, I am your father."
+#     if name == "Leia":
+# 		    return "Luke, I am your sister."
+#     if name == "Han":
+# 		    return "Luke, I am your brother in law."
+#     if name == "R2D2":
+# 		    return "Luke, I am your droid."
+
+
+# 2-VAZIFA
+# berilgan ismlarni alifbo tartibida tartiblash!
+
+# def get_student_names(students):
+# 	s = list(students.values())
+# 	return sorted(s)
+
+
+# 3-VAZIFA
+# ?
