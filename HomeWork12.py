3a2f75c216a2f12ab5e1126a87123cd3409e90d7
Barchinoy <126873220+NBarchinoy@users.noreply.github.com>
2023-03-30 09:47:25 +0500

    Add files via upload

---
+++ HomeWork12.py
@@ -0,0 +1,22 @@
+# def ispalindrome(sonlar):
+#     if sonlar in range(1, 10):
+#         elif sonlar = (555, 556)
+#         elif sonlar = (878, 898)
+# ispalindrome = int(sonlar)
+# return ()
+
+# count_palindromes(1, 10) ➞ 9
+
+# count_palindromes(555, 556) ➞ 1
+
+# count_palindromes(878, 898) ➞ 3
+
+
+# 2-vazifa
+# def hacker_speak(suz):
+#   suz = suz.replace("a", "4")
+#   suz = suz.replace("e", "3")
+#   suz = suz.replace("i", "1")
+#   suz = suz.replace("o", "0")
+#   suz = suz.replace("s", "5")
+#   return suz
