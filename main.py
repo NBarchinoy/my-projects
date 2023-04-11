b6df4ec0a7809bc0e234489d7accd4b33226a68d
Barchinoy <126873220+NBarchinoy@users.noreply.github.com>
2023-04-11 09:32:05 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# def XO(t):
+#   l = t.lower()
+#   return l.count('x') == l.count('o')
+
+
+# 2-topshiriq
+# import re
+
+# pattern = "red flag|blue flag"
+
+
+# 3-topshiriq
+# def count_vowels(txt):
+#   result = 0
+#   for letter in txt:
+#     if letter in "aeiouAEIOU":
+#       result+=1
+#   return result
+
+
+# 4-topshiriq
+# import re
+# def replace_vowels(txt, ch):
+#   return re.sub(r'[aeiouAEIOU]', ch, tx
+
+
+# 5-topshiriq
+# import re
+# pattern = r"[aA].*[pP].*[aA]"
